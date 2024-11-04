7908507e7653b1d3e46de3444843246393035186
Gihzx <145726882+Gihzx@users.noreply.github.com>
2024-11-04 15:04:10 -0300

    feat: input e radio adicionado

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import { Segment } from 'semantic-ui-react';
-import './App.css';
-import FormProduto from './views/produto/FormProduto';
-import FormEntregador from './views/Entregador/FormEntregador';
+import { Segment } from "semantic-ui-react";
+import "./App.css";
+import FormProduto from "./views/produto/FormProduto";
+import FormEntregador from "./views/Entregador/FormEntregador";
+import FormCliente from "./views/cliente/FormCliente";
 function App() {
   return (
-    <><FormProduto/>
-    <FormEntregador/>
-     <div style={{marginTop: '6%'}}>
-        <Segment vertical color='grey' size='tiny' textAlign='center'>
+    <>
+      <FormCliente />
+      <FormProduto />
+      <FormEntregador />
+      <div style={{ marginTop: "6%" }}>
+        <Segment vertical color="grey" size="tiny" textAlign="center">
           &copy; 2023 - Projeto WEB III - IFPE Jaboatão dos Guararapes
         </Segment>
       </div>
-</>
-    
+    </>
   );
 }
 
--- src/views/Entregador/FormEntregador.jsx
+++ src/views/Entregador/FormEntregador.jsx
@@ -1,198 +1,124 @@
-import InputMask from 'react-input-mask';
-import { Button, Container, Divider, Form, Icon,Select} from 'semantic-ui-react';
+import InputMask from "react-input-mask";
+import {
+  Button,
+  Container,
+  Divider,
+  Form,
+  Icon,
+  Dropdown,
+  Radio,
+} from "semantic-ui-react";
 
-function FormEntregador(){
-    const countryOptions = [
-        { key: 'af', value: 'af', text: 'Afghanistan' },
-        { key: 'ax', value: 'ax', text: 'Aland Islands' },
+function FormEntregador() {
+  const options = [
+    { key: "af", value: "af", text: "Afghanistan" },
+    { key: "ax", value: "ax", text: "Aland Islands" },
+  ];
 
-    
-      ]
-      
-    return(
-        <>
-        <div>
+  return (
+    <>
+      <div>
+        <div style={{ marginTop: "3%" }}>
+          <Container textAlign="justified">
+            <h2>
+              {" "}
+              <span style={{ color: "darkgray" }}>
+                Entregador&nbsp;
+                <Icon name="angle double right" size="small" />{" "}
+              </span>{" "}
+              Cadastro{" "}
+            </h2>
 
-<div style={{marginTop: '3%'}}>
+            <Divider />
 
-   <Container textAlign='justified' >
+            <div style={{ marginTop: "4%" }}>
+              <Form>
+                <Form.Group widths="equal">
+                  <Form.Input required fluid label="Nome" maxLength="100" />
 
-       <h2> <span style={{color: 'darkgray'}}>Entregador&nbsp;<Icon name='angle double right' size="small" /> </span> Cadastro </h2>
-
-       <Divider />
-
-       <div style={{marginTop: '4%'}}>
-
-           <Form>
-
-               <Form.Group widths='equal'>
-
-                   <Form.Input
-                       required
-                       fluid
-                       label='Nome'
-                       maxLength="100"
-                   />
-
-                   <Form.Input
-                       required
-                       fluid
-                       width={10}
-                       label='CPF'>
-                       <InputMask
-                           required
-                           mask="999.999.999-99"
-
-                       /> 
-                   </Form.Input>
-                   <Form.Input
-                       required
-                       fluid
-                       width={10}
-                       label='RG'>
-                       <InputMask
-                           required
-                           mask="999.999.999-99"
-
-                       /> 
-                   </Form.Input>
-
-               </Form.Group>
-             
-               <Form.Group widths='equal'>
-
-               <Form.Input
-                    
-                       fluid
-                       width={10}
-                       label='DT Nascimento'>
-                       <InputMask
-                           required
-                           mask="999.999.999-99"
-
-                       /> 
-                   </Form.Input>
-                   <Form.Input
-                       required
-                       fluid
-                       width={10}
-                       label='Fone Celular'>
-                       <InputMask
-                           required
-                           mask="999.999.999-99"
-
-                       /> 
-                   </Form.Input>
-                   <Form.Input
-
-                       fluid
-                       width={10}
-                       label='Fone Fixo'>
-                   
-                   </Form.Input>
-                   <Form.Input
-                      
-                       fluid
-                       width={10}
-                       label='QTD Entregas Realizada'>
-                     
-                   </Form.Input>
-                   <Form.Input
-                      
-                       fluid
-                       width={10}
-                       label='Valor Por Frente'>
-                    
-                   </Form.Input>
-
-               </Form.Group>
-            <Form.Group>
-            <Form.Input
-                      
-                      fluid
-                      width={10}
-                      label='Rua'>
-                   
+                  <Form.Input required fluid width={10} label="CPF">
+                    <InputMask required mask="999.999.999-99" />
                   </Form.Input>
+                  <Form.Input fluid width={10} label="RG"></Form.Input>
+                </Form.Group>
+
+                <Form.Group widths="equal">
+                  <Form.Input fluid width={10} label="DT Nascimento">
+                    <InputMask required mask="999.999.999-99" />
+                  </Form.Input>
+                  <Form.Input required fluid width={10} label="Fone Celular">
+                    <InputMask required mask="999.999.999-99" />
+                  </Form.Input>
+                  <Form.Input fluid width={10} label="Fone Fixo"></Form.Input>
                   <Form.Input
-                      
-                      fluid
-                      width={6}
-                      label='Número'>
-                   
-                  </Form.Input>
-            </Form.Group>
-            <Form.Group>
-            <Form.Input
-                      
-                      fluid
-                      width={10}
-                      label='Bairro'>
-                   
-                  </Form.Input>
+                    fluid
+                    width={10}
+                    label="QTD Entregas Realizada"
+                  ></Form.Input>
                   <Form.Input
-                      
-                      fluid
-                      width={6}
-                      label='Cidade'>
-                   
-                  </Form.Input>
-                  <Form.Input
-                      
-                      fluid
-                      width={6}
-                      label='CEP'>
-                   
-                  </Form.Input>
-                
-            </Form.Group>
-            <Select placeholder='Select your country' options={countryOptions}  width={16} />
-           <Form.Group>
+                    fluid
+                    width={10}
+                    label="Valor Por Frente"
+                  ></Form.Input>
+                </Form.Group>
+                <Form.Group>
+                  <Form.Input fluid width={10} label="Rua"></Form.Input>
+                  <Form.Input fluid width={6} label="Número"></Form.Input>
+                </Form.Group>
+                <Form.Group>
+                  <Form.Input fluid width={10} label="Bairro"></Form.Input>
+                  <Form.Input fluid width={6} label="Cidade"></Form.Input>
+                  <Form.Input fluid width={6} label="CEP"></Form.Input>
+                </Form.Group>
+                <label>UF</label>
+                <Dropdown
+                  placeholder="Select"
+                  fluid
+                  multiple
+                  selection
+                  label="UF"
+                  options={options}
+                />
+                <Form.Group>
+                  <Form.Input fluid width={10} label="Complemento"></Form.Input>
+                </Form.Group>
+                <Form.Group>
+                  <h4 style={{ marginRight: "10px" }}>Ativo : </h4>
+                  <Radio label="Sim" style={{ marginRight: "10px" }} />
+                  <Radio label="Não" />
+                </Form.Group>
+              </Form>
 
-           <Form.Input
-                       required
-                       fluid
-                       width={10}
-                       label="Complemento">
-                   </Form.Input>
-               
-               </Form.Group>
-           </Form>
-           
-           <div style={{marginTop: '4%'}}>
+              <div style={{ marginTop: "4%" }}>
+                <Button
+                  type="button"
+                  inverted
+                  circular
+                  icon
+                  labelPosition="left"
+                  color="orange"
+                >
+                  <Icon name="reply" />
+                  Voltar
+                </Button>
 
-               <Button
-                   type="button"
-                   inverted
-                   circular
-                   icon
-                   labelPosition='left'
-                   color='orange'
-               >
-                   <Icon name='reply' />
-                   Voltar
-               </Button>
-                   
-               <Button
-                   inverted
-                   circular
-                   icon
-                   labelPosition='left'
-                   color='blue'
-                   floated='right'
-               >
-                   <Icon name='save' />
-                   Salvar
-               </Button>
-
-           </div>
-
-       </div>
-       
-   </Container>
-</div>
-</div>
-       </>
-    )
+                <Button
+                  inverted
+                  circular
+                  icon
+                  labelPosition="left"
+                  color="blue"
+                  floated="right"
+                >
+                  <Icon name="save" />
+                  Salvar
+                </Button>
+              </div>
+            </div>
+          </Container>
+        </div>
+      </div>
+    </>
+  );
 }
-export default FormEntregador
+export default FormEntregador;
